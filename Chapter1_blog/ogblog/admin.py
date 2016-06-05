25b454729103c07a1a4905e107923347ba9cd903
oglop <1oglop1@gmail.com>
2016-06-05 12:28:25 +0200

    Added Post model, and tuned admin.py

--- Chapter1_blog/ogblog/admin.py
+++ Chapter1_blog/ogblog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+
+# class PostAdmin(admin.AdminSite):
+#   list_display = ('title', 'slug', 'author', 'date_published', 'status')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'date_published', 'status')
+    list_filter = ('status', 'date_created', 'date_published', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = { 'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'date_published'
+    ordering = ['status', 'date_published']
+
+
+admin.site.register(Post, PostAdmin)
--- Chapter1_blog/ogblog/models.py
+++ Chapter1_blog/ogblog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='date_published')
+    body = models.TextField()
+    author = models.ForeignKey(User, related_name='blog_posts')
+    date_created = models.DateTimeField(default=timezone.now)
+    date_published = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default='draft')
+
+    class Meta:
+        ordering = ('-date_published',)
+
+    def __str__(self):
+        return self.title
