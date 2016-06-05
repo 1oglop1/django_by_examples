ae66a72fa7f32692f3e7970b8518ea193df2b54a
oglop <1oglop1@gmail.com>
2016-06-05 13:43:04 +0200

    views.py <-- added post_list, post_details

--- Chapter1_blog/ogblog/models.py
+++ Chapter1_blog/ogblog/models.py
@@ -2,14 +2,24 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
-class Post(models.Model):
+class DatePublishedManager(models.Manager):
+    """manager to get posts which are published"""
 
+    def get_queryset(self):
+        return super(DatePublishedManager, self).get_queryset().filter(status='published')
+
+        #return super().get_queryset().filter(status='published')
+
+
+class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
+
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='date_published')
@@ -19,6 +29,10 @@ class Post(models.Model):
     date_published = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     status = models.CharField(choices=STATUS_CHOICES, max_length=10, default='draft')
+
+    # query manager
+    objects = models.Manager()  # default manager
+    published = DatePublishedManager() # published manager
 
     class Meta:
         ordering = ('-date_published',)
--- Chapter1_blog/ogblog/views.py
+++ Chapter1_blog/ogblog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.published.all()
+
+    return render(request, 'ogblog/post/list.html', {'posts': posts})
+
+def post_details(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='published',
+                             published__year=year,
+                             publshed_month=month,
+                             published_day=day)
+
+    return render(request, 'ogblog/post/detail.html', {'post':post})
